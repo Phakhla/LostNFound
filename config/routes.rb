810bd7c3a5dbba261af39566bf7dbcb903d24f39
kanchanit <kanchanit@bananacoding.com>
2022-05-30 12:32:49 +0700

    get old version

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :founds
-  resources :losts
-  # get 'home/indezx'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  # root "articles#index"
 end
