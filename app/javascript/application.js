a6dc9dca84efc2e51edc66d4fb48a31886673c79
tontonton <tawatchai@bananacoding.com>
2022-07-07 06:09:05 +0000

    Merged PR 10067: Remove font awesome free
    Remove font awesome free

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,12 @@
 import 'bootstrap';
 import $ from 'jquery';
 
-import '@fortawesome/fontawesome-free/js/all';
 import './controllers';
 import '@hotwired/turbo-rails';
 import * as ActiveStorage from '@rails/activestorage';
 
 ActiveStorage.start();
 window.$ = $;
-
-FontAwesome.config.mutateApproach = 'sync';
 
 window.dispatchMapsEvent = function dispatchMapsEvent(...args) {
   const event = new CustomEvent('google-maps-callback');
