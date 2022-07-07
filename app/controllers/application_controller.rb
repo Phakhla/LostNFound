c5c42abc95962aadd6a17daf43c9ba8a7d987fe1
Rinradee Chaipaisan <Rinradee@bananacoding.com>
2022-07-07 03:22:30 +0000

    Merged PR 10061: nonloginuser login then after login back to previous page
    ![image.png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image.png)
    ![image (2).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%282%29.png)
    ![image (3).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%283%29.png)
    ![image (4).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%284%29.png)
    ![image (5).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%285%29.png)
    ![image (6).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%286%29.png)
    ![image (7).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10061/attachments/image%20%287%29.png)
    
    Related work items: #11130

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username firstname lastname tel email password
@@ -12,8 +13,16 @@ class ApplicationController < ActionController::Base
                                                                 not_show_address ])
   end
 
-  def after_sign_in_path_for(_resource)
-    root_path
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
   end
 
   def after_sign_out_path_for(_resource)
