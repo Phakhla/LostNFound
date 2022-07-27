b41dd1b30c1c741998ea9c8fe3f77c26e72d2402
Kanchanit Bunma <Kanchanit@bananacoding.com>
2022-07-27 03:08:38 +0000

    Merged PR 10196: Redesign post image
    ![image.png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10196/attachments/image.png)
    
    Scrolling
    ![image (2).png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10196/attachments/image%20%282%29.png)
    
    Related work items: #11174

--- app/javascript/controllers/post_image_controller.js
+++ app/javascript/controllers/post_image_controller.js
@@ -3,10 +3,26 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['image'];
 
-  connect() {}
+  connect() { }
 
   removeImage(e) {
     this.imageTarget.remove();
     e.preventDefault();
+  }
+
+  slideLeft() {
+    document.getElementById('gallery-scroll').scrollBy({
+      top: 0,
+      left: -180,
+      behavior: 'smooth',
+    });
+  }
+
+  slideRight() {
+    document.getElementById('gallery-scroll').scrollBy({
+      top: 0,
+      left: +180,
+      behavior: 'smooth',
+    });
   }
 }
