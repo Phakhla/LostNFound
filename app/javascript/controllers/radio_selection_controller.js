25bc5dd07424723f0c8aff6145f73bbf1a60e600
Aunyamanee Pukkaew <Aunyamanee@bananacoding.com>
2022-07-27 04:07:28 +0000

    Merged PR 10199: เลือก อื่นๆ ควร validate
    ![ภาพถ่ายหน้าจอ 2565-07-27 เวลา 10.05.50.png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10199/attachments/%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%88%E0%B8%AD%202565-07-27%20%E0%B9%80%E0%B8%A7%E0%B8%A5%E0%B8%B2%2010.05.50.png) ![ภาพถ่ายหน้าจอ 2565-07-26 เวลา 15.44.11.png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10199/attachments/%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%88%E0%B8%AD%202565-07-26%20%E0%B9%80%E0%B8%A7%E0%B8%A5%E0%B8%B2%2015.44.11.png)
    
    Related work items: #11272

--- app/javascript/controllers/radio_selection_controller.js
+++ app/javascript/controllers/radio_selection_controller.js
@@ -5,16 +5,26 @@ export default class extends Controller {
 
   connect() { }
 
-  checkOption() {
+  checkOption(e) {
     if (this.foundRadioButtonTarget.checked) {
       this.reasonInputTarget.value = this.foundRadioButtonTarget.value;
       this.statusInputTarget.value = 'found';
     } else if (this.hopelessRadioButtonTarget.checked) {
       this.reasonInputTarget.value = this.hopelessRadioButtonTarget.value;
       this.statusInputTarget.value = 'closed';
-    } else {
+    } else if (this.textInputTarget.value) {
       this.reasonInputTarget.value = this.textInputTarget.value;
       this.statusInputTarget.value = 'closed';
+    } else {
+      $('#SelectionAlert').addClass('is-invalid');
+      e.preventDefault();
+    }
+  }
+
+  checkOther() {
+    const $textInput = $('#SelectionAlert');
+    if (this.textInputTarget.value !== '') {
+      $textInput.removeClass('is-invalid');
     }
   }
 }
