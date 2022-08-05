fe00e03aed6ea61602f3302106899f8c176580d2
Pisit Lerditthivej <pisit@bananacoding.com>
2022-08-05 14:44:08 +0700

    change branch in cd and capistrano

--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -2,7 +2,7 @@
 
 set :application, 'lostitem_uat'
 server '172.104.175.170', user: 'deploy', roles: %w[app db web]
-set :branch, ENV['BRANCH'] || 'main'
+set :branch, ENV['BRANCH'] || 'uat'
 set :nginx_server_name, 'lostandfound-uat.onbananacoding.com'
 set :deploy_to, '/srv/www/apps/lostitem-uat'
 set :puma_service_unit_name, "puma_#{fetch(:application)}"
