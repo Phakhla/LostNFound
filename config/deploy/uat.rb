48a4a713612a00bcc8388d040eba82d5196c9dd0
Pisit Lerditthivej <pisit@bananacoding.com>
2022-08-05 11:11:25 +0700

    add config uat site and cd

---
+++ config/deploy/uat.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+set :application, 'lostitem_uat'
+server '172.104.175.170', user: 'deploy', roles: %w[app db web]
+set :branch, ENV['BRANCH'] || 'main'
+set :nginx_server_name, 'lostandfound-uat.onbananacoding.com'
+set :deploy_to, '/srv/www/apps/lostitem-uat'
+set :puma_service_unit_name, "puma_#{fetch(:application)}"
+# Postgres create db
+set :pg_ask_for_password, true # Prompts user for password on execution of setup
