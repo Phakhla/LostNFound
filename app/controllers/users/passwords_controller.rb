7055285e2af9a8c6042deb92daba98a58be0aeee
Aunyamanee Pukkaew <Aunyamanee@bananacoding.com>
2022-09-01 05:07:53 +0000

    Merged PR 10558: forgot password token is invalid
    ![Screen Shot 2565-09-01 at 10.58.37.png](https://dev.azure.com/bananacoding/83e999a8-2fc3-4699-862d-4cf3a9c7f6c9/_apis/git/repositories/90290335-3a9b-4811-8590-982990f48542/pullRequests/10558/attachments/Screen%20Shot%202565-09-01%20at%2010.58.37.png)
    
    Related work items: #11730

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -10,6 +10,17 @@ class Users::PasswordsController < Devise::PasswordsController
     end
   end
 
+  def edit
+    original_token = params[:reset_password_token]
+    reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
+    self.resource = resource_class.find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
+
+    return unless resource.errors.empty?
+
+    flash[:alert] = 'Password token is invalid'
+    redirect_to new_session_path(resource_name)
+  end
+
   protected
 
   def load_email
